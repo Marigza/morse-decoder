b4612997b4b65e9ab98dac2f2b930d9c67548451
Marigza <marigola@mail.ru>
2023-01-19 17:39:20 +0300

    feat: create morse-decoder function

--- src/index.js
+++ src/index.js
@@ -34,11 +34,36 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+    '-----': '0',
+    '     ': ' ',
 };
 
 function decode(expr) {
     // write your solution here
+    let array = expr.split('');
+    let subarray = [];
+    let result = [];
+
+    for (let i = 0; i < array.length / 10; i++) {
+        subarray[i] = array.slice((i * 10), (i * 10) + 10);
+        let arrayDouble = [];
+        let morseArr = [];
+
+        for (let j = 0; j < subarray[i].length / 2; j++) {
+            arrayDouble[j] = subarray[i].slice((j * 2), (j * 2) + 2).join('');
+            if (arrayDouble[j] === '00') {
+                morseArr.push('')
+            } else if (arrayDouble[j] === '10') {
+                morseArr.push('.')
+            } else if (arrayDouble[j] === '11') {
+                morseArr.push('-')
+            } else if (arrayDouble[j] === '**') {
+                morseArr.push(' ')
+            }
+        }
+        result.push(MORSE_TABLE[morseArr.join('')])
+    }
+    return result.join('')
 }
 
 module.exports = {
